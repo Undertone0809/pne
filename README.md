2c9e82b2c858e393e6a2069e20f4e2e1448f47d4
zeeland <zeeland4work@gmail.com>
2024-05-19 03:38:25 +0800

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# pne
+
+<div align="center">
+
+
+[![Python Version](https://img.shields.io/pypi/pyversions/pne.svg)](https://pypi.org/project/pne/)
+[![Dependencies Status](https://img.shields.io/badge/dependencies-up%20to%20date-brightgreen.svg)](https://github.com/Undertone0809/pne/pulls?utf8=%E2%9C%93&q=is%3Apr%20author%3Aapp%2Fdependabot)
+[![Semantic Versions](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--versions-e10079.svg)](https://github.com/Undertone0809/pne/releases)
+[![License](https://img.shields.io/github/license/Undertone0809/pne)](https://github.com/Undertone0809/pne/blob/main/LICENSE)
+
+
+🚀 Simplified promptulate call. Now you can use `import pne` to call [promptulate](https://github.com/Undertone0809/promptulate).
+
+</div>
+
+## Quick start
+
+Conda package manager is recommended. Create a conda environment.
+
+```bash
+pip install pne
+```
+
+Then you can anything function like promptulate:
+
+```python
+import pne
+
+pne.chat(messages="Hello, world!", model="gpt-4-turbo")
+```
+
+## Credits [![🚀 Your next Python package needs a bleeding-edge project structure.](https://img.shields.io/badge/3PG-%F0%9F%9A%80-brightgreen)](https://github.com/Undertone0809/python-package-template)
+
+This project was generated with [3PG](https://github.com/Undertone0809/3PG)
---
+++ pne/__init__.py
@@ -0,0 +1,62 @@
+import warnings
+
+from promptulate.agents.assistant_agent.agent import AssistantAgent
+from promptulate.agents.base import BaseAgent
+from promptulate.agents.planner.planner import Planner
+from promptulate.agents.tool_agent.agent import ToolAgent
+from promptulate.agents.web_agent.agent import WebAgent
+from promptulate.chat import AIChat, chat
+from promptulate.llms.base import BaseLLM
+from promptulate.llms.factory import LLMFactory
+from promptulate.llms.openai.openai import ChatOpenAI
+from promptulate.output_formatter import OutputFormatter
+from promptulate.schema import (
+    AssistantMessage,
+    BaseMessage,
+    MessageSet,
+    SystemMessage,
+    UserMessage,
+)
+from promptulate.tools.base import BaseTool, Tool, define_tool
+from promptulate.utils.logger import enable_log
+from promptulate.utils.string_template import StringTemplate
+
+_util_fields = [
+    "enable_log",
+    "OutputFormatter",
+    "StringTemplate",
+]
+
+_schema_fields = [
+    "AssistantMessage",
+    "SystemMessage",
+    "UserMessage",
+    "BaseMessage",
+    "MessageSet",
+]
+
+_llm_fields = ["chat", "AIChat", "BaseLLM", "ChatOpenAI", "LLMFactory"]
+
+_tool_fields = [
+    "Tool",
+    "define_tool",
+    "BaseTool",
+]
+
+_agent_fields = [
+    "BaseAgent",
+    "WebAgent",
+    "ToolAgent",
+    "AssistantAgent",
+    "Planner",
+]
+
+__all__ = [
+    *_util_fields,
+    *_schema_fields,
+    *_llm_fields,
+    *_tool_fields,
+    *_agent_fields,
+]
+
+warnings.filterwarnings("always", category=DeprecationWarning)
